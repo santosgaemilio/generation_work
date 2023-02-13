6b2db98d6c0a4ee7a0f8406180fc150e40a004a0
emilio_santos <santosgaemilio@gmail.com>
2023-02-12 23:05:25 -0600

    Tarea INTRO-WEB-07

---
+++ TAREAS/INTRO-WEB-7/js/ejercicio1.js
@@ -0,0 +1,4 @@
+function change_name(){
+    let name = prompt("What is your name")
+    document.getElementById("name").innerHTML = name;
+}
---
+++ TAREAS/INTRO-WEB-7/js/ejercicio2.js
@@ -0,0 +1,7 @@
+function boton1(){
+    alert("En esta página se encuentran 3 pokemones tipo fantasma!!")
+}
+
+function boton2(){
+    alert("Gengar es un Pokémon de tipo fantasma/veneno introducido en la primera generación. Es la evolución de Haunter. ")
+}
