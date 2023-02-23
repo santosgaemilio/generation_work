0de9d89ccf4f4f4729ce14f92312be9e5b000cf7
emilio_santos <santosgaemilio@gmail.com>
2023-02-23 17:36:21 -0600

    Primeros ejercicios de Java

---
+++ 05-JAVA/JAVA-01/Java-Terminal/HolaMundo.java
@@ -0,0 +1,14 @@
+//import Saludo.
+// Realmente no se necesita import
+// Para llamar a todo el programa con todo y la clase saludo no necesitas correr HolaMundo.java
+// con un java HolaMundo tienes
+
+class HolaMundo{
+	public static void main(String [] args){
+		
+
+		Saludo objSaludo;
+		objSaludo = new Saludo();
+		objSaludo.saludar();
+	}
+}
---
+++ 05-JAVA/JAVA-01/Java-Terminal/Saludo.java
@@ -0,0 +1,8 @@
+class Saludo
+{
+	void saludar()
+	{
+		System.out.println("Hola chavalitos bienvenidos a mi canal");
+		System.out.println("Hola de nuez chavalitos");
+	}
+}
