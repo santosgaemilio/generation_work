90219f3c727704f2a08c5fb77e4b9f6d32c8c62c
emilio_santos <santosgaemilio@gmail.com>
2023-03-22 17:11:54 -0600

    Java web

---
+++ 07-JAVA-WEB/JW-01-BROWSER/demo01/src/test/java/com/generation/cohorte23/Demo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.cohorte23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
