d7767e29f2e476b82cb0a57144acc6956505afb5
emilio_santos <santosgaemilio@gmail.com>
2023-03-28 20:17:06 -0600

    Eclipse

---
+++ 07-JAVA-WEB/JW-FELIPE/ecommerce/src/main/java/com/generation/ecommerce/model/Producto.java
@@ -0,0 +1,76 @@
+package com.generation.ecommerce.model;
+
+public class Producto {
+	// Attributes
+	private Long id;
+	private String nombre;
+	private String descripcion;
+	private String URL_Imagen;
+	private double precio;
+	
+
+	// Constructor vacio para el Jackson (serializar u deserealizar un
+	// objeto JAVA a JSON)
+	public Producto() {
+		
+	}
+
+	//Constructor con todos los campos
+	public Producto(Long id, String nombre, String descripcion, String URL_Imagen, double precio) {
+		
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.URL_Imagen = URL_Imagen;
+		this.precio = precio;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getURL_Imagen() {
+		return URL_Imagen;
+	}
+
+	public void setURL_Imagen(String uRL_Imagen) {
+		URL_Imagen = uRL_Imagen;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", URL_Imagen="
+				+ URL_Imagen + ", precio=" + precio + "]";
+	}
+	
+	
+	
+}
